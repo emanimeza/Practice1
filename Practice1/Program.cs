ae2ced44d6d893dca7d69048cfbb5406de36f0ab
Emanuel <emanimeza@gmail.com>
2019-11-20 18:52:47 -0500

    added word

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -6,7 +6,7 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); word 
         }
     }
 }
